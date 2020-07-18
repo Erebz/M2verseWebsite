61eafa2082b3df086ef44aba12b9f163003e27d2
Yacine <yacine.hamdi@etu.univ-lyon1.fr>
2020-07-18 21:04:23 +0200

    [WIP] - Page welcome ajoutée

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,24 @@
+@extends('template')
+
+{!! Html::style('styles/style_welcome.css') !!}
+
+@section('contenu')
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+        <h1 class="display-3">M2verse.</h1>
+        <p class="lead">Create. Share. Like never before.</p>
+        <hr class="my-4">
+            <h1 class="display-5 author font-weight-light">Staff</h1>
+            <div class="row">
+                <div class="col-6">
+                    <p class="author font-weight-light">Jennie - dev back-end</p>
+                    <img src="img/jennie1.jpg"/>
+                </div>
+                <div class="col-6">
+                    <p class="author font-weight-light">Lisa - dev front-end</p>
+                    <img src="img/lisa1.jpg"/>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     {!! Html::style('lib/bootstrap/bootstrap.css') !!}
+    {!! Html::style('styles/style_welcome.css') !!}
 
     <title>@yield('titrePage')</title>
 </head>
@@ -11,14 +12,14 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
 
-        <a class="navbar-brand" href="{{url('/communautes')}}">M2verse</a>
+        <a class="navbar-brand" href="{{url('/')}}">M2verse</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="{{url('/communautes')}}">Home
+                    <a class="nav-link" href="{{url('/')}}">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
@@ -49,6 +50,6 @@
 </footer>
 {!! Html::script('lib/jquery/jquery-3.5.1.js') !!}
 {!! Html::script('lib/js/bootstrap.bundle.js') !!}
-{!! Html::script('lib/js/bootstrap.js/bootstrap.js') !!}
+{!! Html::script('lib/js/bootstrap.js') !!}
 </body>
 </html>
